43c6e7fc234e202febd79fc83900137dde2bede7
KrushnaAsDeveloper <krushnawaghmare165@gmail.com>
2026-07-11 15:39:14 +0530

    fix: correct typo in minlength property and ensure proper import formatting in user model

--- Backend/src/model/user.model.js
+++ Backend/src/model/user.model.js
@@ -1,6 +1,7 @@
 import { Timestamp } from "mongodb";
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 const userSchema = mongoose.Schema({
     email : {
         type : String,
@@ -19,11 +20,34 @@ const userSchema = mongoose.Schema({
     password :{
         type : String,
         required : true, 
-        minlenght : 6
+        minlength : 6
 
     }
-}, {Timestamp : true})
+}, {timestamps : true})
 
- const User = mongoose.model("User", userSchema);
+userSchema.pre("save", async function() {
+    if(!this.isModified("password")) return 
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+userSchema.methods.comparePassword = async function(password) {
+    return bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.genrateAccessToken = function() {
+    return jwt.sign(
+        {
+        _id : this._id, 
+        username : this.username, 
+        email : this.email,
+        
+    },  
+    process.env.ACCESS_TOKEN_SECRET, 
+    {
+        expiresIn : process.env.ACCESS_TOKEN_EXPIREY
+    }
+    )
+}
+const User = mongoose.model("User", userSchema);
 
 export default User;
