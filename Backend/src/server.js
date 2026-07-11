3838b146bbf15113c13a6756395678d1c3e587fd
KrushnaAsDeveloper <krushnawaghmare165@gmail.com>
2026-07-11 10:45:40 +0530

    feat: add multer middleware for file uploads and integrate Cloudinary for image storage

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,13 +7,14 @@ import Listing from "./model/listing.model.js";
 import User from "./model/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import path from "path"
 import { protect } from "./middlewares/user.middleware.js";
 import listingsRouter from "./routes/listings.routes.js";
 import userRouter from "./routes/user.routes.js"
 import { db_connection } from "./db/db.js";
 const port = 5000;
 const app = express();
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(express.urlencoded({ extended: true, limit : "20kb" }));
 app.use(express.json({limit : "20kb"}));
---
+++ Backend/src/utils/cloudinary.service.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs, { rmSync } from "fs"
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUD_API_KEY, 
+  api_secret: process.env.CLOUD_API_SECRATE,
+//   upload_prefix: `cloudinary://${process.env.CLOUD_API_KEY}:${process.env.CLOUD_API_SECRATE}@${process.env.CLOUD_NAME}?secure_distribution=mydomain.com&upload_prefix=https://api-eu.cloudinary.com
+// `
+});
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try {
+        if(!localFilePath) return null
+    const response = await cloudinary.uploader.upload(localFilePath, {resource_type : "auto"})
+    
+    console.log("file upload successfully ")
+    return response
+    } catch (error) {
+          fs.unlinkSync(localFilePath)      
+          return null
+    }
+
+}
+
+
+export {uploadOnCloudinary}
