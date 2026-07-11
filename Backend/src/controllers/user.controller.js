06fed57bdb6322c041029745f9ff2db16c74e13b
KrushnaAsDeveloper <krushnawaghmare165@gmail.com>
2026-07-11 15:36:18 +0530

    feat: refactor user registration logic to include input validation and error handling

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -2,75 +2,25 @@ import express from "express";
 import User from "../model/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-export const Register = async (req, res)=>{
-    try {
-        // get all the inputs from body
-        const {email, username, password } = req.body;
-// check for email duplications
-    const duplicate = await User.findOne({email})
-    if(duplicate){
-       return res.status(400).json({msg : "email already existed!"})
-    }
-// hashed the password using bcrypt.hash
-    const hashedPassword = await bcrypt.hash(password ,10);
-// create and saved the new user by saving the password in the hasedform
-    const newUser = new User({email, username,  password : hashedPassword})
-    await newUser.save().then(res=>{
-        console.log(res);
-        
-    })
-// make a token 
-    const token = jwt.sign(
-        {userId : newUser._id, email : newUser.email}, 
-        process.env.JWT_SECRET, 
-        {expiresIn : "7d"}
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/ApiError.js"
 
-    )
-// now send the token back to the 
-    res.status(201).json({
-        
-        token, 
-        user :{userId : newUser._id, username : newUser.username, email : newUser.email}
-    }
-    )
-    } catch (error) {
-         res.status(500).json({ message: "Server error", error })
-    }
+export const registerUser = asyncHandler(async (req, res) => {
+  const { email, username, password } = req.body;
+  
+  if([email, username, password].some((field)=> field?.trim() == "")){
+    throw new ApiError(400, "All feilds are required !")
+  }
 
+  const existedUser = await User.findOne({$or:[{username}, {email}]})
+  if(existedUser){
+    throw new ApiError(400, "User existed!");
     
-}
+  }
 
-export const Login = async (req, res)=>{
-   try {
-     const findUser = await User.findOne({email : req.body.email})
-    if(!findUser){
-       return res.status(400).json({
-            msg : "email not found !"
-        })
-    }
+  const user=  await User.create({email, username, password})
+  console.log(user)
+  console.log(Error.message)
 
-    const isMatched = await bcrypt.compare(req.body.password, findUser.password)
-    if(!isMatched){
-       return res.status(400).json({
-            msg : "Password is invalid !"
-        })
-    }
-    
-    const token = jwt.sign(
-        {userId:findUser._id, email : findUser.email, username : findUser.username}, 
-        process.env.JWT_SECRET, 
-        {expiresIn : "7d"}
-    )
-
-    res.status(201).json({
-        token, 
-        user :{userId : findUser._id, username : findUser.username, email : findUser.email}
-        
-    })
-   } catch (error) {
-    res.status(400).json({
-        msg : "invalid inpute !"
-    })
-   }
-    
-}
+  res.status(200).json({done : "User Registerd !"})
+});
