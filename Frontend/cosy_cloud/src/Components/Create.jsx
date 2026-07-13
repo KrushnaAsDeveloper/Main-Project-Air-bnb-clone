c9f24c29138a47861b28e3040f51860130ebbed6
KrushnaAsDeveloper <krushnawaghmare165@gmail.com>
2026-07-13 18:17:48 +0530

    feat: import centralized axios instance in Create component

--- Frontend/cosy_cloud/src/Components/Create.jsx
+++ Frontend/cosy_cloud/src/Components/Create.jsx
@@ -1,5 +1,6 @@
   import { useState } from "react";
   import { useNavigate } from "react-router-dom";
+  import api from '../api/axios.api';
   import axios from "axios";
   export default function Create() {
     let navigate = useNavigate();
