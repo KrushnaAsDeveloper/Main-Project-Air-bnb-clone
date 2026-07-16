5d33fe8d55dd72f84cf379931752d066b91404a9
KrushnaAsDeveloper <krushnawaghmare165@gmail.com>
2026-07-16 23:52:58 +0530

    feat: add loading spinner to Create and Update buttons for better user feedback

--- Frontend/cosy_cloud/src/Components/Create.jsx
+++ Frontend/cosy_cloud/src/Components/Create.jsx
@@ -130,7 +130,7 @@
         className="w-full bg-green-600 hover:bg-green-700 text-white font-semibold py-3 rounded-lg transition"
         
       >
-        {loading ? "Creating..." : "Create Listing"}
+        {loading ? <span className=' flex justify-center items-center'><svg width={25} fill="#FFFFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><circle cx="4" cy="12" r="3" opacity="1"><animate id="spinner_qYjJ" begin="0;spinner_t4KZ.end-0.25s" attributeName="opacity" dur="0.75s" values="1;.2" fill="freeze"/></circle><circle cx="12" cy="12" r="3" opacity=".4"><animate begin="spinner_qYjJ.begin+0.15s" attributeName="opacity" dur="0.75s" values="1;.2" fill="freeze"/></circle><circle cx="20" cy="12" r="3" opacity=".3"><animate id="spinner_t4KZ" begin="spinner_qYjJ.begin+0.3s" attributeName="opacity" dur="0.75s" values="1;.2" fill="freeze"/></circle></svg></span> : "Create Listing"}
       </button>
     </div>
   </form>
--- Frontend/cosy_cloud/src/Components/Update.jsx
+++ Frontend/cosy_cloud/src/Components/Update.jsx
@@ -49,7 +49,7 @@ function Update() {
     fd.append("image",  form.image)
     await axios.put(`/api/listings/${id}`, fd );  
     setForm({
-        name: "",
+    name: "",
     description: "",
     country: "",
     location: "",
@@ -144,7 +144,7 @@ function Update() {
       className="w-full bg-green-600 hover:bg-green-700 text-white font-semibold py-3 rounded-lg transition"
     >
       {
-        loading ? "Updating.."  : "Update Listing"
+        loading ? <span className=' flex justify-center items-center'><svg width={25} fill="#FFFFFFFF" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><circle cx="4" cy="12" r="3" opacity="1"><animate id="spinner_qYjJ" begin="0;spinner_t4KZ.end-0.25s" attributeName="opacity" dur="0.75s" values="1;.2" fill="freeze"/></circle><circle cx="12" cy="12" r="3" opacity=".4"><animate begin="spinner_qYjJ.begin+0.15s" attributeName="opacity" dur="0.75s" values="1;.2" fill="freeze"/></circle><circle cx="20" cy="12" r="3" opacity=".3"><animate id="spinner_t4KZ" begin="spinner_qYjJ.begin+0.3s" attributeName="opacity" dur="0.75s" values="1;.2" fill="freeze"/></circle></svg></span>  : "Update Listing"
       }
     </button>
   </div>
